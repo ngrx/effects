775eb03f9c789ea50d0ecdf6143263d6f24d2555
Mike Ryan <mike.ryan52@gmail.com>
2016-05-09 12:04:42 -0500

    Initial implementation

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # @ngrx/effects
 ### Side effect model for @ngrx/store
 [![Join the chat at https://gitter.im/ngrx/store](https://badges.gitter.im/ngrx/store.svg)](https://gitter.im/ngrx/store?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+
+## Effects
+In @ngrx/effects, effects are simply _sources of actions_. You use the `@Effect()` decorator to hint which observables on a service are action sources, and @ngrx/effects automatically connects your action sources to your store
+
+To help you compose new action sources, @ngrx/effects exports a StateChanges observable service that emits every time your state changes along with the action that caused the state change.
+
+For example, here's an AuthEffects service that describes a source of login actions:
+```ts
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Action } from '@ngrx/store';
+import { StateChanges, Effect } from '@ngrx/effects'
+
+@Injectable()
+export class AuthEffects {
+  @Effect() login$: Observable<Action>;
+
+  constructor(http: Http, changes$: StateChanges) {
+    this.login$ = changes$
+      // Listen for the 'LOGIN' action to occur
+      .filter(change => change.action.type === 'LOGIN')
+      // Map the payload into JSON to use as the request body
+      .map(change => JSON.stringify(change.action.payload))
+      .switchMap(payload => http.post('/auth', payload)
+        // If successful, dispatch success action with result
+        .map(res => ({ type: 'LOGIN_SUCCESS', payload: res.json() }))
+        // If request fails, dispatch failed action
+        .catch(() => Observable.of({ type: 'LOGIN_FAILED' }));
+      );
+  }
+}
+```
+
+Then you run your effects during bootstrap:
+```ts
+import { runEffects } from '@ngrx/effects';
+
+bootstrap(App, [
+  provideStore(reducer),
+  runEffects(AuthEffects)
+]);
+```
--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,97 @@
+/// <reference path="../node_modules/reflect-metadata/reflect-metadata.d.ts"/>
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/withLatestFrom';
+import 'rxjs/add/operator/observeOn';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/cache';
+import { Action, Dispatcher, State } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { OpaqueToken, APP_INITIALIZER, provide, Provider } from '@angular/core';
 
+const METADATA_KEY = '@ngrx/effects';
+
+export function Effect(): PropertyDecorator {
+  return function(target: any, propertyName: string) {
+    if (!Reflect.hasOwnMetadata(METADATA_KEY, target)) {
+      Reflect.defineMetadata(METADATA_KEY, [], target);
+    }
+
+    const effects = Reflect.getOwnMetadata(METADATA_KEY, target);
+
+    Reflect.defineMetadata(METADATA_KEY, [ ...effects, propertyName ], target);
+  };
+}
+
+export function getEffectKeys(instance: any): string[] {
+  const target = Object.getPrototypeOf(instance);
+
+  if (!Reflect.hasOwnMetadata(METADATA_KEY, target)) {
+    return [];
+  }
+
+  return Reflect.getOwnMetadata(METADATA_KEY, target);
+}
+
+export function flatten(list: any[]): any[] {
+  return list.reduce((items: any[], next) => {
+    if (Array.isArray(next)) {
+      return items.concat(flatten(next));
+    }
+
+    return items.concat(next);
+  }, []);
+}
+
+export function mergeEffects(...instances: any[]): Observable<any> {
+  const observables = instances.map(i => getEffectKeys(i).map(key => i[key]));
+
+  return Observable.merge(...flatten(observables));
+}
+
+export interface StateChange {
+  state: any;
+  action: Action;
+}
+
+export class StateChanges extends Observable<StateChange> { }
+
+export const BOOTSTRAP_EFFECTS = new OpaqueToken('@ngrx/effects Bootstrap Effects');
+
+
+export function connectEffectsToDispatcher(dispatcher: Dispatcher, effects: any[]) {
+  return function() {
+    mergeEffects(...effects).subscribe(dispatcher);
+
+    return Promise.resolve(true);
+  }
+}
+
+export const STATE_CHANGES_PROVIDER = provide(StateChanges, {
+  deps: [ Dispatcher, State ],
+  useFactory(actions$: Dispatcher, state$: State<any>) {
+    return actions$
+      .withLatestFrom(state$)
+      .map<StateChange>(([ action, state ]) => ({ state, action }))
+      .cache();
+  }
+});
+
+
+export const CONNECT_EFFECTS_PROVIDER = provide(APP_INITIALIZER, {
+  multi: true,
+  deps: [ Dispatcher, BOOTSTRAP_EFFECTS ],
+  useFactory: connectEffectsToDispatcher
+});
+
+export function runEffects(...effects: any[]) {
+  const allEffects = flatten(effects).map(effect => provide(BOOTSTRAP_EFFECTS, {
+    useClass: effect,
+    multi: true
+  }));
+
+  return [
+    ...allEffects,
+    CONNECT_EFFECTS_PROVIDER,
+    STATE_CHANGES_PROVIDER
+  ];
+}
