15e9312625c10b488ad49f648b95796d7d6ef5f0
Mike Ryan <mike.ryan52@gmail.com>
2016-05-11 07:04:45 -0500

    Fix import for bootstrap effects token

--- lib/run-effects.ts
+++ lib/run-effects.ts
@@ -1,10 +1,9 @@
 import { OpaqueToken, Provider } from '@angular/core';
 
 import { flatten } from './util';
-import { CONNECT_EFFECTS_PROVIDER } from './effects';
+import { CONNECT_EFFECTS_PROVIDER, BOOTSTRAP_EFFECTS } from './effects';
 import { STATE_UPDATES_PROVIDER } from './state-updates';
 
-export const BOOTSTRAP_EFFECTS = new OpaqueToken('@ngrx/effects Bootstrap Effects');
 
 export function runEffects(...effects: any[]) {
   const allEffects = flatten(effects).map(effect => new Provider(BOOTSTRAP_EFFECTS, {
