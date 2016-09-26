d5189072f9e0b2e7c4b22761ff39e9b35aec99a4
Mike Ryan <mike.ryan52@gmail.com>
2016-09-26 18:24:07 -0500

    chore(build): Update to latest ngrx build process

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { Effect, mergeEffects } from './effects';
-export { Actions } from './actions';
-export { EffectsModule } from './module';
-export { EffectsSubscription } from './effects-subscription';
