1ec737ba7806ec82a6b238f12a4f9bf26f298485
Mike Ryan <mike.ryan52@gmail.com>
2016-09-26 19:11:37 -0500

    chore(build): Reorganize project structure

--- testing/testing.module.ts
+++ testing/testing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Actions } from '@ngrx/effects';
-import { EffectsTestRunner } from './runner';
+import { EffectsRunner } from './runner';
 
 
-export function _createActions(runner: EffectsTestRunner): Actions {
+export function _createActions(runner: EffectsRunner): Actions {
   return new Actions(runner);
 }
 
 @NgModule({
   providers: [
-    EffectsTestRunner,
-    { provide: Actions, deps: [ EffectsTestRunner ], useFactory: _createActions }
+    EffectsRunner,
+    { provide: Actions, deps: [ EffectsRunner ], useFactory: _createActions }
   ]
 })
 export class EffectsTestingModule { }
