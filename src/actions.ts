ac44aaff8178ef5cf4c9a8e3ba6622850f03352a
Kurt <k.aquilina92@gmail.com>
2017-07-10 12:06:23 +0200

    feat(actions): group of type
    Wait for multiple actions before running effect

--- src/actions.ts
+++ src/actions.ts
@@ -29,4 +29,33 @@ export class Actions extends Observable<Action> {
       return false;
     });
   }
+
+  groupOfType(...keys: string[]): Actions {
+    const len = keys.length;
+    let completedActions: { [key: string]: boolean } = null;
+
+    return filter.call(this, ({ type }: { type: string }) => {
+      if (len > 1 && !completedActions) {
+        completedActions = keys.reduce((obj: { [key: string]: boolean }, key: string) => {
+          obj[key] = false;
+          return obj;
+        }, {});
+      }
+
+      if (len === 1) {
+        return type === keys[0];
+      } else if (!completedActions[type] && keys.indexOf(type) > -1) {
+        completedActions[type] = true;
+        const isAllCompleted = Object.keys(completedActions).every(key => completedActions[key]);
+
+        if (isAllCompleted) {
+          completedActions = null;
+        }
+
+        return isAllCompleted;
+      }
+
+      return false;
+    });
+  }
 }
